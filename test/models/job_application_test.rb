42f5fb50af1b81b23e1797e18da9ab7fd186c8d4
o-Ha-minor <tuba1042855@yahoo.co.jp>
2026-05-11 13:59:48 +0900

    バリデーションの追加、テストの追加

--- test/models/job_application_test.rb
+++ test/models/job_application_test.rb
@@ -4,4 +4,30 @@ class JobApplicationTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "valid fixture" do
+    assert job_applications(:one).valid?
+  end
+  test "company_name is required" do
+    job_application = job_applications(:one)
+
+    assert_not job_application.valid?
+  end
+
+  test "interest_level must be included in levels" do
+    job_application = job_applications(:one)
+
+    job_application.interest_level = 6
+
+    assert_not job_application.valid?
+  end
+
+  test "first_interview_on must be after applied_on" do
+    job_application = job_applications(:one)
+
+    job_application.applied_on = Date.new(2026, 5, 10)
+
+    job_application.first_interview_on = Date.new(2026, 5, 1)
+
+    assert_not job_application.valid?
+  end
 end
